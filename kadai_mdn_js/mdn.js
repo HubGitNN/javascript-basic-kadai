34e4f22af6bcd5c561f03ab8ae141d779d25be19
HubGitNN <koichi.nagasaki24@gmail.com>
2025-08-05 14:13:22 +0900

    Add files via upload

---
+++ kadai_mdn_js/mdn.js
@@ -0,0 +1,6 @@
+const today = new Date(); 
+today_year = today.getFullYear();
+today_month = today.getMonth() + 1;
+today_date = today.getDate();
+
+console.log(today_year + '年' + today_month + '月' + today_date + '日');
