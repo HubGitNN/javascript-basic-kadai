b9321237b4445f782d7da94bf2f65ac52922f96c
HubGitNN <koichi.nagasaki24@gmail.com>
2025-08-07 15:37:59 +0900

    Add files via upload

---
+++ kadai_020/event.js
@@ -0,0 +1,9 @@
+// btnというidを持つHTML要素を取得し、定数に代入する
+const btn = document.getElementById('btn');
+
+// HTML要素がクリックされたときにイベント処理を実行する
+btn.addEventListener('click', () => {
+  const childList = document.getElementById('text');
+  childList.textContent = 'ボタンをクリックしました';
+  parentList.appendChild(childList);
+});
