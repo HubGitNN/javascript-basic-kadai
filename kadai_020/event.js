43c5bad1e0b958b45510e5df4d72b762fb7a190a
HubGitNN <koichi.nagasaki24@gmail.com>
2025-08-07 15:47:39 +0900

    Add files via upload

--- kadai_020/event.js
+++ kadai_020/event.js
@@ -5,5 +5,4 @@ const btn = document.getElementById('btn');
 btn.addEventListener('click', () => {
   const childList = document.getElementById('text');
   childList.textContent = 'ボタンをクリックしました';
-  parentList.appendChild(childList);
 });
